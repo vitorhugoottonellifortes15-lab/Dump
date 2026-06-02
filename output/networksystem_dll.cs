f4c4c118675e3a0a6a67d3a3cad504b9217ab84f
vitorhugoottonellifortes15-lab <jogosantigosvh@gmail.com>
2026-06-01 22:27:06 -0300

    1

--- output/networksystem_dll.cs
+++ output/networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-29 01:01:49.123261200 UTC
+// 2026-06-02 01:26:39.526443800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
