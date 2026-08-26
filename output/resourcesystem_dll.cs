63091c37047fa5824213ed622141fe60a38900f8
vitorhugoottonellifortes15-lab <jogosantigosvh@gmail.com>
2026-08-26 18:43:28 -0300

    1

--- output/resourcesystem_dll.cs
+++ output/resourcesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-25 04:13:53.661126700 UTC
+// 2026-08-26 21:43:15.277901300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: resourcesystem.dll
--- output/worldrenderer_dll.rs
+++ output/worldrenderer_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-25 04:13:53.661126700 UTC
+// 2026-08-26 21:43:15.277901300 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
