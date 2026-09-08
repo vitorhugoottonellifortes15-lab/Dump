97d474f08893641246c1afe8a986f9fad19962f8
vitorhugoottonellifortes15-lab <jogosantigosvh@gmail.com>
2026-09-07 21:14:08 -0300

    1

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-09-03 23:36:28.073510400 UTC
+// 2026-09-08 00:13:53.067279300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
